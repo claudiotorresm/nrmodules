1f11ca5254cee1e6aafb6c24f4ec5aa335df86e0
psvensson <psvensson@gmail.com>
2015-11-27 10:34:23 +0100

    foo

---
+++ picklist/1009-PICKLIST.js
@@ -0,0 +1,19 @@
+
+module.exports = function(RED) {
+    "use strict";
+
+    function CheerioNode(n) {
+        RED.nodes.createNode(this,n);
+
+        var node = this;
+        this.on("input", function(msg) {
+            if (msg.hasOwnProperty("payload")) {
+	            node.warn("I saw a payload: "+msg.payload);
+	            // in this example just send it straight on... should process it here really
+	            node.send(msg);
+            }
+            else { node.send(msg); } // If no payload - just pass it on.
+        });
+    }
+    RED.nodes.registerType("picklist",CheerioNode);
+}
